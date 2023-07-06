a7562f953711b2c75ed9b924fa64fc8c139d37ed
Your Name <you@example.com>
2023-07-06 11:36:03 +0200

    deposit/withraw

--- src/MiniYearn.sol
+++ src/MiniYearn.sol
@@ -1,28 +1,40 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.19;
 
+import "@aave/misc/interfaces/IWrappedTokenGatewayV3.sol";
+import "@solmate/tokens/ERC20.sol";
+
 // 0xD322A49006FC828F9B5B37Ab215F99B4E5caB19C -> WETHGateway
 
-interface IWETHGateway{
-    function depositETH(address onBehalfOf, uint16 referralCode) external payable;
-    function withdrawETH(uint256 amount, address onBehalfOf) external;
-}
+contract MiniYearn is ERC20{
 
-contract MiniYearn {
+    constructor() ERC20("Nombre", "SYBL", 18) {
+    }
 
     error CantidadIncorrecta();
 
     function deposit() public payable {
         if (msg.value == 0) revert CantidadIncorrecta();
 
-
-
-
-
-
-
+        IWrappedTokenGatewayV3(0xD322A49006FC828F9B5B37Ab215F99B4E5caB19C)
+        .depositETH{value:msg.value}(
+            0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2, //direccion de la Pool
+            address(this)                               //direccion del que va a recivir los tokens de pool de aave
+            , 0                                         //no queremos codigo de refereidos
+            );
+        _mint(msg.sender, msg.value);
     }
 
+    function withraw(uint256 amount) public {
+        if (amount > balanceOf[msg.sender]) revert CantidadIncorrecta();
 
-
+        IWrappedTokenGatewayV3(0xD322A49006FC828F9B5B37Ab215F99B4E5caB19C)
+        .withdrawETH(
+            0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2, //direccion de la Pool
+            amount,                                    //cantidad a retirar
+            msg.sender                                  //Persona que los va a recivir
+        );
+        
+        _burn(msg.sender, amount);
+    }
 }
